047ce4e713e68cc17e8b3899343759aa979c9d9d
Florian Hars <florian@hars.de>
2013-02-16 18:07:03 +0100

    actually implement the A* algorithm

--- AStar.ml
+++ AStar.ml
@@ -14,6 +14,46 @@ module Make(G: GraphWithHeuristic) : sig
     (int * G.node * G.edge list * int) option
 end = struct
 
-  let solve _ _ = failwith "Not implemented"
+  module Seen = Set.Make(struct
+    type t = G.node
+    let compare = G.compare_nodes
+  end)
 
+  let rec solve_rec fringe seen problem =
+    match Heap.head fringe with
+      | Some(heuristic, cost, node, path) ->
+          if G.solved node problem then
+            Some(cost, node, List.rev path, Seen.cardinal seen)
+          else
+            let fringe' = Heap.remhead fringe in
+            let fringe'', seen' =
+              if not (Seen.mem node seen) then
+                let seen' = Seen.add node seen
+                and edges = G.edges node problem in
+                let fringe'' =
+                  List.fold_left
+                    (fun fringe (c, n, e) ->
+                      if Seen.mem n seen' then
+                        fringe
+                      else
+                        if Seen.mem n seen' then
+                          fringe'
+                        else
+                          let cost' = cost + c in
+                          let estimate = cost' + G.heuristic n problem in
+                          Heap.add fringe (estimate, cost', n, e::path)
+                    ) fringe' edges in
+                fringe'', seen'
+              else
+                fringe', seen
+            in
+            solve_rec fringe'' seen' problem
+      | None ->
+          None
+
+  let solve state problem =
+    let estimate = G.heuristic state problem in
+    let fringe = Heap.singleton (estimate, 0, state, [])
+    and seen = Seen.empty in
+    solve_rec fringe seen problem
 end
